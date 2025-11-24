0c83422c718eee5e9e81dd452f6727a9a4357f53
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2025-11-24 12:38:15 -0500

    disabled route to change password page

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import Login from './Login.jsx'
 import Register from './Register.jsx'
-import ChangePassword from './ChangePassword.jsx'
+// import ChangePassword from './ChangePassword.jsx'
 
 const AuthContextProvider = ({ children }) => {
   const [accessToken, setAccessToken] = useState('')
@@ -49,7 +49,7 @@ createRoot(document.getElementById('root')).render(
             <Route path="/" element={<App />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/changepassword" element={<ChangePassword />} />
+            {/* <Route path="/changepassword" element={<ChangePassword />} /> */}
           </Routes>
         </Router>
       </UserContextProvider>
